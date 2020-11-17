75e3363975e864f256ee4fa751a610f7a111608d
Alexander J McKenzie <AMCKEN205@caledonian.ac.uk>
2020-11-17 03:15:56 +0000

    commit3

--- data_group_sub_genres.py
+++ data_group_sub_genres.py
@@ -22,21 +22,11 @@ genre_group_list = \
 
 for key, top200 in top200s.items():
     # locate the apperance of the given genre grouping strings and replace as required
-    top200.loc[top200["artist_top_genre"].str.contains(genre_group_list[0]), "artist_top_genre"] = genre_group_list[0]
-    top200.loc[top200["artist_top_genre"].str.contains(genre_group_list[1]), "artist_top_genre"] = genre_group_list[1]
-    top200.loc[top200["artist_top_genre"].str.contains(genre_group_list[2]), "artist_top_genre"] = genre_group_list[2]
-    top200.loc[top200["artist_top_genre"].str.contains(genre_group_list[3]), "artist_top_genre"] = genre_group_list[3]
-    top200.loc[top200["artist_top_genre"].str.contains(genre_group_list[4]), "artist_top_genre"] = genre_group_list[4]
-    top200.loc[top200["artist_top_genre"].str.contains(genre_group_list[5]), "artist_top_genre"] = genre_group_list[5]
-    top200.loc[top200["artist_top_genre"].str.contains(genre_group_list[6]), "artist_top_genre"] = genre_group_list[6]
-    top200.loc[top200["artist_top_genre"].str.contains(genre_group_list[7]), "artist_top_genre"] = genre_group_list[7]
-    top200.loc[top200["artist_top_genre"].str.contains(genre_group_list[8]), "artist_top_genre"] = genre_group_list[8]
-    top200.loc[top200["artist_top_genre"].str.contains(genre_group_list[9]), "artist_top_genre"] = genre_group_list[9]
-    top200.loc[top200["artist_top_genre"].str.contains(genre_group_list[10]), "artist_top_genre"] = genre_group_list[10]
-    top200.loc[top200["artist_top_genre"].str.contains(genre_group_list[11]), "artist_top_genre"] = genre_group_list[11]
-    top200.loc[top200["artist_top_genre"].str.contains(genre_group_list[12]), "artist_top_genre"] = genre_group_list[12]
-    top200.loc[top200["artist_top_genre"].str.contains(genre_group_list[13]), "artist_top_genre"] = genre_group_list[13]
-    top200.loc[top200["artist_top_genre"].str.contains(genre_group_list[14]), "artist_top_genre"] = genre_group_list[14]
+    i = 0
+    while i < len(genre_group_list):
+        top200.loc[top200["artist_top_genre"].str.contains(genre_group_list[i]), "artist_top_genre"] = genre_group_list[i]
+        i += 1
+
     # handle some special cases
     top200.loc[top200["artist_top_genre"].str.contains("beatlesque"), "artist_top_genre"] = genre_group_list[0]
     # Artist BROCKHAMPTON is the only general rap artist labelled as boyband. 
